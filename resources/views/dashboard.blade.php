9024e3220aeba1a6bf102a5c5cc56c1efc2da343
AsadullohRuziev <asadullohruziev@gmail.com>
2022-11-25 19:18:37 +0500

    manager design side finished with TailwindCSS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,57 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     @if(auth()->user()->role->name === 'manager')
-                        You're Manager!
+                        <span class="text-lg text-blue-500 font-bold">Recieved Applications</span>
+                        <!-- component -->
+                        <!-- This is an example component -->
+                        <div class='mt-5'>
+                            <div class="rounded-xl border p-5 mt-5 shadow-md w-9/12 bg-white">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+                                        <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">ID: 158001</button>
+                                        <div class="text-xs text-neutral-500">2022-10-30</div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-3">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+                                    <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        client@company.com
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="rounded-xl border p-5 mt-5 shadow-md w-9/12 bg-white">
+                                <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+                                        <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">ID: 158001</button>
+                                        <div class="text-xs text-neutral-500">2022-10-30</div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-4 mb-3">
+                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
+                                    <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        client@company.com
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     @elseif(auth()->user()->role->name === 'client')
                         You're Client!
                     @endif

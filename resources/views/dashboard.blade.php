509defa6164fcb68b374c5da6f2c83ee7886945f
AsadullohRuziev <asadullohruziev@gmail.com>
2022-11-25 19:55:14 +0500

    Clent's design side finish

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,7 +62,28 @@
                             </div>
                         </div>
                     @elseif(auth()->user()->role->name === 'client')
-                        You're Client!
+                        <!-- component -->
+                        <div class='flex items-center justify-center min-h-screen'>
+                            <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+                                <div class='max-w-md mx-auto space-y-6'>
+
+                                    <form action="" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <h2 class="text-2xl font-bold ">Submit your application</h2>
+                                        <hr class="my-6">
+                                        <label class="uppercase text-sm font-bold opacity-70">Subject</label>
+                                        <input type="text" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded">
+                                        <label class="uppercase text-sm font-bold opacity-70">Message</label>
+                                        <textarea rows="5" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none" ></textarea>
+                                        <label class="uppercase text-sm font-bold opacity-70">File</label>
+                                        <input type="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded">
+
+                                        <input type="submit" class="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300" value="Send">
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
                     @endif
                 </div>
             </div>
